526c265173849f39c747e553edfb1e02b0619434
afarroc <108610976+afarroc@users.noreply.github.com>
2026-06-07 04:16:51 -0500

    feat(postventa): add forms and admin configuration
    - SeguimientoBOForm, EstadoDespachoForm, EstadoCourierForm
    - ProveedorForm for provider management
    - Register models in admin with list_display and filters

---
+++ apps/postventa/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PostventaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.postventa'
+    verbose_name = 'Postventa'

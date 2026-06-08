1cbf4b916b3293dad4da5b410f15f2285bdbcb93
afarroc <108610976+afarroc@users.noreply.github.com>
2026-06-08 00:34:46 -0500

    fix(postventa): importar UpdateView en views.py

--- apps/postventa/views.py
+++ apps/postventa/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
